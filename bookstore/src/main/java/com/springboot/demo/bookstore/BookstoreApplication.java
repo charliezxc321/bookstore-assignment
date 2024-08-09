17470251e9016c7ec1396ad0616d4af7bb614f89
Charlie Teo <charlieteo@Charlies-MacBook-Pro.local>
2024-08-09 15:12:58 +0800

    Added properties and annotation to integrate with eureka server and api gateway, changed port to 8090

--- bookstore/src/main/java/com/springboot/demo/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/com/springboot/demo/bookstore/BookstoreApplication.java
@@ -2,8 +2,10 @@ package com.springboot.demo.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
